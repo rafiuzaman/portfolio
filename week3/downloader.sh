61b2a0a5a905a2000d643b1ad9d34d4fcd76c093
Rafi Zaman <rafi.zaman@ecu.edu.au>
2021-07-25 06:52:05 +0000

    Added downloader.sh

--- week3/downloader.sh
+++ week3/downloader.sh
@@ -3,8 +3,6 @@
 # prompt the user to type a download location
 # use wget to download the webpage requested by the user
 # repeat the process until the user types “exit”.
-# Example 
-# wget -P /mnt/iso http://mirrors.mit.edu/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1804.iso
 
 #!/bin/bash
 
